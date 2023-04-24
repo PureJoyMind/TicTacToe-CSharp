e8143ef1899c7a8a9c5fd7bcc2ad15bc815bfc87
PureJoyMind <mazdak.parniandg@gmail.com>
2023-04-25 00:55:50 +0430

    Added methods. has bugs

--- Board.cs
+++ Board.cs
@@ -1,13 +1,40 @@
-﻿namespace TicTacToe
+﻿using System;
+
+namespace TicTacToe
 {
     public class Board
     {
-        private enum State
+        private char[] spaces = new char[9] { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+
+        public int NumMoves;
+
+        public bool UpdateBoard(int move, char symbol)
         {
-            X,
-            O
+            if (move < 1 || move > 9)
+            {
+                return false;
+            }
+            else if (spaces[move - 1] != " ")
+            {
+                return false;
+            }
+            else
+            {
+                spaces[move - 1] = symbol;
+                NumMoves++;
+                return true;
+            }
         }
 
-        private State Spaces;
+        public void DisplayBoard()
+        {
+            Console.WriteLine($" {spaces[0]} | {spaces[1]} | {spaces[2]} ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($" {spaces[3]} | {spaces[4]} | {spaces[5]} ");
+            Console.WriteLine("---+---+---");
+            Console.WriteLine($" {spaces[6]} | {spaces[7]} | {spaces[8]} ");
+        }
+
+
     }
 }
--- Player.cs
+++ Player.cs
@@ -3,12 +3,12 @@
     public class Player
     {
         public string Name { get; private set; }
-        public char Role { get; private set; }
+        public char Symbol { get; private set; }
 
-        public Player(string name, char role)
+        public Player(string name, char symbol)
         {
             Name = name;
-            Role = role;
+            Symbol = symbol;
         }
     }
 }
